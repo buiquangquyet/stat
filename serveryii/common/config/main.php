eed7c793062d61edf0736a31ba3fdd6b2c5d1750
Quyetbq <quyetbq@peacesoft.net>
2018-10-27 11:17:58 +0700

    dev 27-11

--- serveryii/common/config/main.php
+++ serveryii/common/config/main.php
@@ -6,20 +6,20 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-            'keyPrefix' => 'Weshop',
-        ],
 //        'cache' => [
-//            'class' => 'yii\caching\MemCache',
-//            'servers' => [
-//                [
-//                    'host' => '127.0.0.1',
-//                    'port' => 11211,
-//                    'weight' => 100,
-//                ],
-//            ],
+//            'class' => 'yii\caching\FileCache',
+//            'keyPrefix' => 'Weshop_',
 //        ],
+        'cache' => [
+            'class' => 'yii\caching\MemCache',
+            'servers' => [
+                [
+                    'host' => '127.0.0.1',
+                    'port' => 11211,
+                    'weight' => 100,
+                ],
+            ],
+        ],
 
 //        'cache' => [
 //            'class' => 'yii\caching\ApcCache',
