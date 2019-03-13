df993fab1799cd22723a84cda2d543b81de51cc3
bui quang quyet <buiquangquyet@gmail.com>
2019-03-13 12:03:19 +0700

    dev

--- serveryii/console/controllers/DictionaryController.php
+++ serveryii/console/controllers/DictionaryController.php
@@ -25,7 +25,6 @@ class DictionaryController extends Controller
         $listToken['eyAT9VAuYGc:APA91bFIlU4ifu-RDLVwOoezqCiaRKIc-RBv2qRRBxJ7RMgvWoBXX7NkFrQszSHyxOjaMIha4aS2j_vKW01fGA9Kp3BrkjnLQSPYcq3PR0vPb4ZcuhlymYbdlU_eMJMtZGmTP-az_TYi']=1;// quyet
         $listToken['dRi1cg0QDbM:APA91bFtdbVhy7pv6csDA5F2oGskeOL4CSHIZJdZeKzNzCUKUOH1rTOAYObZwPtp4dHyKsps7NGS9rLMSxbsfVEiSad6TD68lhwIIAHp-M1vLORt6nEZDFMh-mJSR9S8-TVcfvt-F8fk']=1;// quyet
         $listToken['dIfe1Np11mo:APA91bFBZ0qNrR4dBPsaRR5FgoZNhbo8FUuJGSbWn3k1lJoN5qPXSgbhqkAQfRLtDOvoHK-ER_2hHHc8FhNh0Fvux7wK0-hlEulKBO474bg8WcnvkcdHtu-bMwgPq0wg77B5_JkhKTcn']=1;// quyet
-        //$listToken['f3ls8x77wEA:APA91bEFbntzvGu40I98WS8KUFvCAm0IXfAk7v7dyL70yurppKXhAcSgs9zWf0s2W_2b3pHwqHHRjfkSa7M_n7qD272p-p6RHQIucmwfuAkGZsxCIcCRFc55uz9fiyQruVcHuHIVm_mX']=1;// quyet
         $listToken['eWs2KuSvGYU:APA91bFMzwXi3THFYD4gkQ0YgV-Y59yGGbqL1t73RdYGY-zvebfTZp2Xi1t138Dwekvd_3nUpRNjcT0-HH3_M2emseqdHxO9T5sZk8atZP4UIJAR6HIQwZYPFijxwbIryGan7ADbYD3h']=2;// hanh
         $listToken['fkDn0cpBSWA:APA91bHsXVgX3bvr429D7uABqNw4ndKvpAItnxHHL78p2Rdw6c9SaFkFnQPgxVC0aegcemKt09YnZ9VHEMB3yadVQPBhDnstKQcl3IAYloZn6zviOvQj9cxWCb7QM28EZIRFB-omAveR']=3;// thuy
         $listToken['c58Me2mi0LI:APA91bHOOKVqKeJwF0Pf4DAjSI4f8nph9t95Sap-_KW7R2xUf4uHCr6JypGC-w8SmDXTfInMHwdSVQm8CALoFbh3k9kwfZ71alDJou82NOb9foBrrWnjRy33jq8TpPnqaiRIsRonACmE-omAveR']=3;// thuy
